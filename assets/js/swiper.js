74663ebcc3dc31c31f32c4515e0f67a6e8e0d2e7
zxlee0114 <napoleon.lee0114@gmail.com>
2024-09-08 00:41:36 +0800

    環境建置 0907

---
+++ assets/js/swiper.js
@@ -0,0 +1,36 @@
+// import Swiper bundle with all modules installed
+// 匯入全部模組
+import Swiper from "swiper/bundle";
+
+// Start
+
+const heroSwiper = new Swiper(".heroSwiper", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  // 左右箭頭
+  navigation: {
+    prevEl: ".heroSwiper-button-prev",
+    nextEl: ".heroSwiper-button-next",
+  },
+  pagination: {
+    el: ".heroSwiper-pagination",
+  },
+
+  watchSlidesProgress: true,
+  breakpoints: {
+    768: {
+      spaceBetween: 15, // slide 間距
+    },
+    992: {
+      spaceBetween: 40, // slide 間距
+    }
+  },
+  // 循環播放
+  loop: true,
+  // 自動撥放
+  autoplay: {
+    delay: 2000,
+  },
+});
+
+// 參考文章：https://blog.csdn.net/qq_42855675/article/details/112850049
